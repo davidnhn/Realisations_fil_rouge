d2766feb2b1a082fd4191b9be6fcf8c34a7aaade
David Nahon <david.nahon@hotmail.fr>
2022-05-09 16:55:30 +0200

    ajout sticky bar

--- ma-recette/script.js
+++ ma-recette/script.js
@@ -1,1 +1,29 @@
-document.querySelector("lottie-player").style.fill = "rgba(0,0,0,0)";
+const navbar = document.querySelector(".header");
+const navbarHeight = navbar.getBoundingClientRect().height;
+const hero = document.querySelector(".hero");
+const stickySearch = document.querySelector(".header__search");
+/* ===============================================
+
+        *STICKY NAVBAR
+
+   ===============================================*/
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    navbar.classList.add("sticky");
+    stickySearch.style.display = "block";
+  } else {
+    navbar.classList.remove("sticky");
+    stickySearch.style.display = "none";
+  }
+};
+
+const heroObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navbarHeight}px`,
+});
+
+heroObserver.observe(hero);
